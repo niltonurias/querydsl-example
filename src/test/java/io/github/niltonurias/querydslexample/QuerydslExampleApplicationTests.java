5ed689f17e551cba049d894c02ceddb5b1948901
Nilton Urias da Cruz Junior <vte_ncruz@uolinc.com>
2021-12-13 21:25:03 -0300

    First commit

---
+++ src/test/java/io/github/niltonurias/querydslexample/QuerydslExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.niltonurias.querydslexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
